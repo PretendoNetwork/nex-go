c1200be0766568176d0db3ad41225bed5a9ceed2
RedDuckss <jonbarrow1998@gmail.com>
2018-08-27 01:50:12 -0400

    v0 checksum tweaks

--- prudp/checksum.go
+++ prudp/checksum.go
@@ -2,13 +2,12 @@ package prudp
 
 import (
 	"encoding/binary"
-	"errors"
+	"fmt"
+	"reflect"
 )
 
-// CalculateChecksum calculates the checksum of a prudpv0 packet
-// checksum: The base of the checksum.
-// packet  : The clients connection state
-func CalculateChecksum(checksum int, packet []byte) (int, error) {
+// CalculateV0PacketChecksum calculates the checksum of a prudpv0 packet
+func CalculateV0PacketChecksum(checksum int, packet []byte) int {
 	pos := 0
 
 	sections := len(packet) / 4
@@ -21,70 +20,61 @@ func CalculateChecksum(checksum int, packet []byte) (int, error) {
 		pos += 4
 	}
 
-	temp1, err := sum(chunks)
-	if err != nil {
-		return 0, err
-	}
+	temp1 := Sum(chunks)
 	temp := temp1 & 0xFFFFFFFF
 
 	buff := make([]byte, 4)
 	binary.LittleEndian.PutUint32(buff, uint32(temp))
 
-	tempSum, err := sum(packet[len(packet) & ^3:])
-	if err != nil {
-		return 0, err
-	}
+	tempSum := Sum(packet[len(packet) & ^3:])
 
 	checksum += tempSum
-	tempSum, err = sum(buff)
-	if err != nil {
-		return 0, err
-	}
+	tempSum = Sum(buff)
 	checksum += tempSum
 
-	return (checksum & 0xFF), nil
+	return (checksum & 0xFF)
 }
 
-func sum(SliceNotAsserted interface{}) (int, error) {
-	sum := 0
+// Sum calculates the sum of the input
+func Sum(a interface{}) int {
+	var (
+		va = reflect.ValueOf(a)
+		r  = float64(0)
+		vb reflect.Value
+	)
 
-	var slice []int
-
-	switch v := SliceNotAsserted.(type) {
-	case []byte:
-		slice = []int{}
-		for _, val := range v {
-			slice = append(slice, int(val))
-		}
-	case []uint32:
-		slice = []int{}
-		for _, val := range v {
-			slice = append(slice, int(val))
-		}
-	default:
-		return 0, errors.New("not a supported type")
+	if va.Kind() != reflect.Slice {
+		panic(fmt.Sprintf("a %s is not a slice!", va.Kind().String()))
 	}
 
-	for _, val := range slice {
-		sum += val
+	for i := 0; i < va.Len(); i++ {
+		vb = va.Index(i)
+
+		switch vb.Kind() {
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			r += float64(vb.Int())
+		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+			r += float64(vb.Uint())
+		case reflect.Float32, reflect.Float64:
+			r += vb.Float()
+		default:
+			panic(fmt.Sprintf("a %s is not a summable type!", vb.Kind().String()))
+		}
 	}
 
-	return sum, nil
+	return int(r)
 }
 
 /*
 func main() {
-	ACCESS_KEY := "9f2b4678"
-	packet := "a1af90000000000000000065d9e3340000"
+	ACCESS_KEY := "ridfebb9"
+	//packet := "a1af90000000000000000065d9e3340000"
 
-	base, _ := sum(ACCESS_KEY)
-	checksum, err := CalculateChecksum(base, []byte(packet)) // Returns as an int
-	if err != nil {
-		fmt.Printf("[err]: unable to calculate checksum for the packet...\n")
-		fmt.Printf("       %v\n", err)
-		os.Exit(1)
-	}
+	base := Sum([]byte(ACCESS_KEY))
+	checksum := CalculateV0PacketChecksum(base, []byte{161, 175, 144, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}) // Returns as an int
 
+	fmt.Println(hex.EncodeToString([]byte{161, 175, 144, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}))
 	fmt.Println(strconv.FormatInt(int64(checksum), 16)) // Print it as a HEX string
+	fmt.Println(checksum)
 }
 */
