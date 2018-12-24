4be4ec432b532ab73be824c6f2feff51eed3d08d
Ezekiel Bethel <zek@9net.org>
2018-12-24 03:52:28 +0000

    NEX stream additions

--- nex_stream.go
+++ nex_stream.go
@@ -95,6 +95,54 @@ func (stream *InputStream) UInt64LE() (ret uint64) {
 	return
 }
 
+// Int8 reads a signed 1 byte integer
+func (stream *InputStream) Int8() (ret int8) {
+	data := stream.Byte()
+	buf := bytes.NewBuffer(data)
+	binary.Read(buf, binary.LittleEndian, &ret)
+	return
+}
+
+// Int16LE reads a signed 2 byte integer in little endian
+func (stream *InputStream) Int16LE() (ret int16) {
+	data := stream.Bytes(2)
+	buf := bytes.NewBuffer(data)
+	binary.Read(buf, binary.LittleEndian, &ret)
+	return
+}
+
+// Int32LE reads a signed 4 byte integer in little endian
+func (stream *InputStream) Int32LE() (ret int32) {
+	data := stream.Bytes(4)
+	buf := bytes.NewBuffer(data)
+	binary.Read(buf, binary.LittleEndian, &ret)
+	return
+}
+
+// Int64LE reads an signed 8 byte integer in little endian
+func (stream *InputStream) Int64LE() (ret int64) {
+	data := stream.Bytes(8)
+	buf := bytes.NewBuffer(data)
+	binary.Read(buf, binary.LittleEndian, &ret)
+	return
+}
+
+// Float32LE reads an signed 4 byte float in little endian
+func (stream *InputStream) Float32LE() (ret float32) {
+	data := stream.Bytes(4)
+	buf := bytes.NewBuffer(data)
+	binary.Read(buf, binary.LittleEndian, &ret)
+	return
+}
+
+// Float64LE reads a 8 byte float in little endian
+func (stream *InputStream) Float64LE() (ret float64) {
+	data := stream.Bytes(8)
+	buf := bytes.NewBuffer(data)
+	binary.Read(buf, binary.LittleEndian, &ret)
+	return
+}
+
 // String reads a NEX string
 func (stream *InputStream) String() string {
 	length := stream.UInt16LE()
@@ -123,6 +171,16 @@ func (stream *InputStream) DataHolder() DataHolder {
 	data.Data = stream.Bytes(int(data.DataLength))
 
 	return data
+}
+
+func (stream *InputStream) Bool() bool {
+	return stream.UInt8() != 0
+}
+
+// TODO: Variant is fucking cursed
+func (stream *InputStream) Variant() (ret Variant) {
+	// fuck you do nothing
+	return
 }
 
 /*
@@ -168,6 +226,53 @@ func (stream *OutputStream) UInt64LE(val uint64) {
 	stream.Write(data)
 }
 
+// Int8 writes a single signed byte to to the buffer
+func (stream *OutputStream) Int8(val int8) {
+	data := new(bytes.Buffer)
+	binary.Write(data, binary.LittleEndian, val)
+	stream.Write(data.Bytes())
+}
+
+// Int16LE writes a 2 byte unsigned integer in little endian
+func (stream *OutputStream) Int16LE(val int16) {
+	data := new(bytes.Buffer)
+	binary.Write(data, binary.LittleEndian, val)
+	stream.Write(data.Bytes())
+}
+
+// Int32LE writes a 4 byte unsigned integer in little endian
+func (stream *OutputStream) Int32LE(val int32) {
+	data := new(bytes.Buffer)
+	binary.Write(data, binary.LittleEndian, val)
+	stream.Write(data.Bytes())
+}
+
+// Int64LE writes a 8 byte unsigned integer in little endian
+func (stream *OutputStream) Int64LE(val int64) {
+	data := new(bytes.Buffer)
+	binary.Write(data, binary.LittleEndian, val)
+	stream.Write(data.Bytes())
+}
+
+// Float32LE reads an signed 4 byte float in little endian
+func (stream *OutputStream) Float32LE(val float32) {
+	data := new(bytes.Buffer)
+	binary.Write(data, binary.LittleEndian, val)
+	stream.Write(data.Bytes())
+}
+
+// Float64LE reads a 8 byte float in little endian
+func (stream *OutputStream) Float64LE(val float64) {
+	data := new(bytes.Buffer)
+	binary.Write(data, binary.LittleEndian, val)
+	stream.Write(data.Bytes())
+}
+
+// TODO: Variant is fucking cursed
+func (stream *OutputStream) Variant(val Variant) {
+	// fuck you do nothing
+}
+
 // String writes a NEX string
 func (stream *OutputStream) String(str string) {
 	if len(str) <= 0 {
@@ -198,4 +303,12 @@ func (stream *OutputStream) QBuffer(buffer []byte) {
 
 	stream.UInt16LE(uint16(length))
 	stream.Write(buffer)
+}
+
+func (stream *OutputStream) Bool(in bool) {
+	if in {
+		stream.UInt8(1)
+	} else {
+		stream.UInt8(0)
+	}
 }
