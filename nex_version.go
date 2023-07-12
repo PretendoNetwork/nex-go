a3b28f6ad287cd541e6590b1bd4bfcb4e201418e
Jonathan Barrow <jonbarrow1998@gmail.com>
2023-07-12 16:25:42 -0400

    Added Copy method to NEXVersion and NewPatchedNEXVersion/NewNEXVersion functions

--- nex_version.go
+++ nex_version.go
@@ -7,3 +7,35 @@ type NEXVersion struct {
 	Patch             int
 	GameSpecificPatch string
 }
+
+// Copy returns a new copied instance of NEXVersion
+func (nexVersion *NEXVersion) Copy() *NEXVersion {
+	var copied *NEXVersion
+
+	if nexVersion.GameSpecificPatch != "" {
+		copied = NewPatchedNEXVersion(nexVersion.Major, nexVersion.Minor, nexVersion.Patch, nexVersion.GameSpecificPatch)
+	} else {
+		copied = NewNEXVersion(nexVersion.Major, nexVersion.Minor, nexVersion.Patch)
+	}
+
+	return copied
+}
+
+// NewPatchedNEXVersion returns a new NEXVersion with a game specific patch
+func NewPatchedNEXVersion(major, minor, patch int, gameSpecificPatch string) *NEXVersion {
+	return &NEXVersion{
+		Major:             major,
+		Minor:             minor,
+		Patch:             patch,
+		GameSpecificPatch: gameSpecificPatch,
+	}
+}
+
+// NewNEXVersion returns a new NEXVersion
+func NewNEXVersion(major, minor, patch int) *NEXVersion {
+	return &NEXVersion{
+		Major: major,
+		Minor: minor,
+		Patch: patch,
+	}
+}
