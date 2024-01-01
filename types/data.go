059da89429791d8853d5c6e65f811f831166a220
Jonathan Barrow <jonbarrow1998@gmail.com>
2023-12-31 22:42:00 -0500

    types: Data e to d

--- types/data.go
+++ types/data.go
@@ -11,13 +11,13 @@ type Data struct {
 }
 
 // WriteTo writes the Data to the given writable
-func (e *Data) WriteTo(writable Writable) {
-	e.WriteHeaderTo(writable, 0)
+func (d *Data) WriteTo(writable Writable) {
+	d.WriteHeaderTo(writable, 0)
 }
 
 // ExtractFrom extracts the Data from the given readable
-func (e *Data) ExtractFrom(readable Readable) error {
-	if err := e.ExtractHeaderFrom(readable); err != nil {
+func (d *Data) ExtractFrom(readable Readable) error {
+	if err := d.ExtractHeaderFrom(readable); err != nil {
 		return fmt.Errorf("Failed to read Data header. %s", err.Error())
 	}
 
@@ -25,36 +25,36 @@ func (e *Data) ExtractFrom(readable Readable) error {
 }
 
 // Copy returns a pointer to a copy of the Data. Requires type assertion when used
-func (e *Data) Copy() RVType {
+func (d *Data) Copy() RVType {
 	copied := NewData()
-	copied.StructureVersion = e.StructureVersion
+	copied.StructureVersion = d.StructureVersion
 
 	return copied
 }
 
 // Equals checks if the input is equal in value to the current instance
-func (e *Data) Equals(o RVType) bool {
+func (d *Data) Equals(o RVType) bool {
 	if _, ok := o.(*Data); !ok {
 		return false
 	}
 
-	return (*e).StructureVersion == (*o.(*Data)).StructureVersion
+	return (*d).StructureVersion == (*o.(*Data)).StructureVersion
 }
 
 // String returns a string representation of the struct
-func (e *Data) String() string {
-	return e.FormatToString(0)
+func (d *Data) String() string {
+	return d.FormatToString(0)
 }
 
 // FormatToString pretty-prints the struct data using the provided indentation level
-func (e *Data) FormatToString(indentationLevel int) string {
+func (d *Data) FormatToString(indentationLevel int) string {
 	indentationValues := strings.Repeat("\t", indentationLevel+1)
 	indentationEnd := strings.Repeat("\t", indentationLevel)
 
 	var b strings.Builder
 
 	b.WriteString("Data{\n")
-	b.WriteString(fmt.Sprintf("%sStructureVersion: %d\n", indentationValues, e.StructureVersion))
+	b.WriteString(fmt.Sprintf("%sStructureVersion: %d\n", indentationValues, d.StructureVersion))
 	b.WriteString(fmt.Sprintf("%s}", indentationEnd))
 
 	return b.String()
