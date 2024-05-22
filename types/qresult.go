8a848c57ba311563d0abc5b21c3d4c98d62e3f75
Jonathan Barrow <jonbarrow1998@gmail.com>
2024-05-22 16:38:42 -0400

    refactor(types): make QResult into a type alias

--- types/qresult.go
+++ types/qresult.go
@@ -8,15 +8,14 @@ import (
 var errorMask = 1 << 31
 
 // QResult is an implementation of rdv::qResult.
+// Type alias of uint32.
 // Determines the result of an operation.
 // If the MSB is set the result is an error, otherwise success
-type QResult struct {
-	Code uint32
-}
+type QResult uint32
 
 // WriteTo writes the QResult to the given writable
-func (r *QResult) WriteTo(writable Writable) {
-	writable.WritePrimitiveUInt32LE(r.Code)
+func (r QResult) WriteTo(writable Writable) {
+	writable.WritePrimitiveUInt32LE(uint32(r))
 }
 
 // ExtractFrom extracts the QResult from the given readable
@@ -26,33 +25,32 @@ func (r *QResult) ExtractFrom(readable Readable) error {
 		return fmt.Errorf("Failed to read QResult code. %s", err.Error())
 	}
 
-	r.Code = code
-
+	*r = QResult(code)
 	return nil
 }
 
 // Copy returns a pointer to a copy of the QResult. Requires type assertion when used
-func (r *QResult) Copy() RVType {
-	return NewQResult(r.Code)
+func (r QResult) Copy() RVType {
+	return &r
 }
 
 // Equals checks if the input is equal in value to the current instance
-func (r *QResult) Equals(o RVType) bool {
+func (r QResult) Equals(o RVType) bool {
 	if _, ok := o.(*QResult); !ok {
 		return false
 	}
 
-	return r.Code == o.(*QResult).Code
+	return r == *o.(*QResult)
 }
 
 // IsSuccess returns true if the QResult is a success
-func (r *QResult) IsSuccess() bool {
-	return int(r.Code)&errorMask == 0
+func (r QResult) IsSuccess() bool {
+	return int(r)&errorMask == 0
 }
 
 // IsError returns true if the QResult is a error
-func (r *QResult) IsError() bool {
-	return int(r.Code)&errorMask != 0
+func (r QResult) IsError() bool {
+	return int(r)&errorMask != 0
 }
 
 // String returns a string representation of the struct
@@ -61,7 +59,7 @@ func (r *QResult) String() string {
 }
 
 // FormatToString pretty-prints the struct data using the provided indentation level
-func (r *QResult) FormatToString(indentationLevel int) string {
+func (r QResult) FormatToString(indentationLevel int) string {
 	indentationValues := strings.Repeat("\t", indentationLevel+1)
 	indentationEnd := strings.Repeat("\t", indentationLevel)
 
@@ -70,9 +68,9 @@ func (r *QResult) FormatToString(indentationLevel int) string {
 	b.WriteString("QResult{\n")
 
 	if r.IsSuccess() {
-		b.WriteString(fmt.Sprintf("%scode: %d (success)\n", indentationValues, r.Code))
+		b.WriteString(fmt.Sprintf("%scode: %d (success)\n", indentationValues, r))
 	} else {
-		b.WriteString(fmt.Sprintf("%scode: %d (error)\n", indentationValues, r.Code))
+		b.WriteString(fmt.Sprintf("%scode: %d (error)\n", indentationValues, r))
 	}
 
 	b.WriteString(fmt.Sprintf("%s}", indentationEnd))
@@ -81,8 +79,9 @@ func (r *QResult) FormatToString(indentationLevel int) string {
 }
 
 // NewQResult returns a new QResult
-func NewQResult(code uint32) *QResult {
-	return &QResult{code}
+func NewQResult(input uint32) *QResult {
+	r := QResult(input)
+	return &r
 }
 
 // NewQResultSuccess returns a new QResult set as a success
