502b6011c62603a67846940c636780676b1185c8
Jonathan Barrow <jonbarrow1998@gmail.com>
2024-05-22 16:31:14 -0400

    refactor(types): make PID into a type alias

--- types/pid.go
+++ types/pid.go
@@ -6,21 +6,17 @@ import (
 )
 
 // PID represents a unique number to identify a user.
-// The official library treats this as a primitive integer.
-//
+// Type alias of uint64.
 // The true size of this value depends on the client version.
-// Legacy clients (WiiU/3DS) use a uint32, whereas modern clients (Nintendo Switch) use a uint64.
-// Value is always stored as the higher uint64, the consuming API should assert accordingly
-type PID struct {
-	pid uint64
-}
+// Legacy clients (Wii U/3DS) use a uint32, whereas modern clients (Nintendo Switch) use a uint64.
+type PID uint64
 
 // WriteTo writes the PID to the given writable
-func (p *PID) WriteTo(writable Writable) {
+func (p PID) WriteTo(writable Writable) {
 	if writable.PIDSize() == 8 {
-		writable.WritePrimitiveUInt64LE(p.pid)
+		writable.WritePrimitiveUInt64LE(uint64(p))
 	} else {
-		writable.WritePrimitiveUInt32LE(uint32(p.pid))
+		writable.WritePrimitiveUInt32LE(uint32(p))
 	}
 }
 
@@ -42,33 +38,32 @@ func (p *PID) ExtractFrom(readable Readable) error {
 		return err
 	}
 
-	p.pid = pid
-
+	*p = PID(pid)
 	return nil
 }
 
 // Copy returns a pointer to a copy of the PID. Requires type assertion when used
-func (p *PID) Copy() RVType {
-	return NewPID(p.pid)
+func (p PID) Copy() RVType {
+	return &p
 }
 
 // Equals checks if the input is equal in value to the current instance
-func (p *PID) Equals(o RVType) bool {
+func (p PID) Equals(o RVType) bool {
 	if _, ok := o.(*PID); !ok {
 		return false
 	}
 
-	return p.pid == o.(*PID).pid
+	return p == *o.(*PID)
 }
 
 // Value returns the numeric value of the PID as a uint64 regardless of client version
-func (p *PID) Value() uint64 {
-	return p.pid
+func (p PID) Value() uint64 {
+	return uint64(p)
 }
 
 // LegacyValue returns the numeric value of the PID as a uint32, for legacy clients
-func (p *PID) LegacyValue() uint32 {
-	return uint32(p.pid)
+func (p PID) LegacyValue() uint32 {
+	return uint32(p)
 }
 
 // String returns a string representation of the struct
@@ -77,20 +72,21 @@ func (p *PID) String() string {
 }
 
 // FormatToString pretty-prints the struct data using the provided indentation level
-func (p *PID) FormatToString(indentationLevel int) string {
+func (p PID) FormatToString(indentationLevel int) string {
 	indentationValues := strings.Repeat("\t", indentationLevel+1)
 	indentationEnd := strings.Repeat("\t", indentationLevel)
 
 	var b strings.Builder
 
 	b.WriteString("PID{\n")
-	b.WriteString(fmt.Sprintf("%spid: %d\n", indentationValues, p.pid))
+	b.WriteString(fmt.Sprintf("%spid: %d\n", indentationValues, p))
 	b.WriteString(fmt.Sprintf("%s}", indentationEnd))
 
 	return b.String()
 }
 
 // NewPID returns a PID instance. The real size of PID depends on the client version
-func NewPID(pid uint64) *PID {
-	return &PID{pid: pid}
+func NewPID(input uint64) *PID {
+	p := PID(input)
+	return &p
 }
