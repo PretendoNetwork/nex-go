d5feb922f41e3170c244b7bb3dbaac60ce2db894
Jonathan Barrow <jonbarrow1998@gmail.com>
2024-05-22 16:26:05 -0400

    refactor(types): make DateTime into a type alias

--- types/datetime.go
+++ types/datetime.go
@@ -7,14 +7,13 @@ import (
 )
 
 // DateTime is an implementation of rdv::DateTime.
+// Type alias of uint64.
 // The underlying value is a uint64 bit field containing date and time information.
-type DateTime struct {
-	value uint64
-}
+type DateTime uint64
 
 // WriteTo writes the DateTime to the given writable
-func (dt *DateTime) WriteTo(writable Writable) {
-	writable.WritePrimitiveUInt64LE(dt.value)
+func (dt DateTime) WriteTo(writable Writable) {
+	writable.WritePrimitiveUInt64LE(uint64(dt))
 }
 
 // ExtractFrom extracts the DateTime from the given readable
@@ -24,28 +23,27 @@ func (dt *DateTime) ExtractFrom(readable Readable) error {
 		return fmt.Errorf("Failed to read DateTime value. %s", err.Error())
 	}
 
-	dt.value = value
-
+	*dt = DateTime(value)
 	return nil
 }
 
 // Copy returns a new copied instance of DateTime
-func (dt *DateTime) Copy() RVType {
-	return NewDateTime(dt.value)
+func (dt DateTime) Copy() RVType {
+	return &dt
 }
 
 // Equals checks if the input is equal in value to the current instance
-func (dt *DateTime) Equals(o RVType) bool {
+func (dt DateTime) Equals(o RVType) bool {
 	if _, ok := o.(*DateTime); !ok {
 		return false
 	}
 
-	return dt.value == o.(*DateTime).value
+	return dt == *o.(*DateTime)
 }
 
 // Make initilizes a DateTime with the input data
 func (dt *DateTime) Make(year, month, day, hour, minute, second int) *DateTime {
-	dt.value = uint64(second | (minute << 6) | (hour << 12) | (day << 17) | (month << 22) | (year << 26))
+	*dt = DateTime(second | (minute << 6) | (hour << 12) | (day << 17) | (month << 22) | (year << 26))
 
 	return dt
 }
@@ -67,39 +65,34 @@ func (dt *DateTime) Now() *DateTime {
 	return dt.FromTimestamp(time.Now().UTC())
 }
 
-// Value returns the stored DateTime time
-func (dt *DateTime) Value() uint64 {
-	return dt.value
-}
-
 // Second returns the seconds value stored in the DateTime
-func (dt *DateTime) Second() int {
-	return int(dt.value & 63)
+func (dt DateTime) Second() int {
+	return int(dt & 63)
 }
 
 // Minute returns the minutes value stored in the DateTime
-func (dt *DateTime) Minute() int {
-	return int((dt.value >> 6) & 63)
+func (dt DateTime) Minute() int {
+	return int((dt >> 6) & 63)
 }
 
 // Hour returns the hours value stored in the DateTime
-func (dt *DateTime) Hour() int {
-	return int((dt.value >> 12) & 31)
+func (dt DateTime) Hour() int {
+	return int((dt >> 12) & 31)
 }
 
 // Day returns the day value stored in the DateTime
-func (dt *DateTime) Day() int {
-	return int((dt.value >> 17) & 31)
+func (dt DateTime) Day() int {
+	return int((dt >> 17) & 31)
 }
 
 // Month returns the month value stored in the DateTime
-func (dt *DateTime) Month() time.Month {
-	return time.Month((dt.value >> 22) & 15)
+func (dt DateTime) Month() time.Month {
+	return time.Month((dt >> 22) & 15)
 }
 
 // Year returns the year value stored in the DateTime
-func (dt *DateTime) Year() int {
-	return int(dt.value >> 26)
+func (dt DateTime) Year() int {
+	return int(dt >> 26)
 }
 
 // Standard returns the DateTime as a standard time.Time
@@ -122,20 +115,21 @@ func (dt *DateTime) String() string {
 }
 
 // FormatToString pretty-prints the struct data using the provided indentation level
-func (dt *DateTime) FormatToString(indentationLevel int) string {
+func (dt DateTime) FormatToString(indentationLevel int) string {
 	indentationValues := strings.Repeat("\t", indentationLevel+1)
 	indentationEnd := strings.Repeat("\t", indentationLevel)
 
 	var b strings.Builder
 
 	b.WriteString("DateTime{\n")
-	b.WriteString(fmt.Sprintf("%svalue: %d (%s)\n", indentationValues, dt.value, dt.Standard().Format("2006-01-02 15:04:05")))
+	b.WriteString(fmt.Sprintf("%svalue: %d (%s)\n", indentationValues, dt, dt.Standard().Format("2006-01-02 15:04:05")))
 	b.WriteString(fmt.Sprintf("%s}", indentationEnd))
 
 	return b.String()
 }
 
 // NewDateTime returns a new DateTime instance
-func NewDateTime(value uint64) *DateTime {
-	return &DateTime{value: value}
+func NewDateTime(input uint64) *DateTime {
+	dt := DateTime(input)
+	return &dt
 }
