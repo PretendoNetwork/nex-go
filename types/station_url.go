ab4dbfcd5251c32cf3d375fb52cc7313e5394e90
Jonathan Barrow <jonbarrow1998@gmail.com>
2024-06-07 17:02:49 -0400

    chore(types): remove pointer from StationURL.SetPrincipalID

--- types/station_url.go
+++ types/station_url.go
@@ -246,8 +246,8 @@ func (s StationURL) NodeID() (uint16, bool) {
 }
 
 // SetPrincipalID sets the stations target PID
-func (s *StationURL) SetPrincipalID(pid *PID) {
-	s.SetParamValue("PID", strconv.FormatUint(uint64(*pid), 10))
+func (s *StationURL) SetPrincipalID(pid PID) {
+	s.SetParamValue("PID", strconv.FormatUint(uint64(pid), 10))
 }
 
 // PrincipalID gets the stations target PID.
